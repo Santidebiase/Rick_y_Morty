79c09e8513efa74f0beb333c5f80d119919091dc
Santidebiase <santidebiase1999@gmail.com>
2022-09-09 20:01:49 -0300

    First Commit

---
+++ src/components/Filters/Category/Status.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import FilterBTN from '../FilterBTN'
+
+const Status = ({ setPageNumber, setStatus }) => {
+
+    let status = ["Alive", "Dead", "Unknow"];
+
+
+
+
+
+
+
+    return (
+        <div className="accordion-item">
+            <h2 className="accordion-header" id="headingThree">
+                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Status
+                </button>
+            </h2>
+            <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div className="accordion-body d-flex flex-wrap gap-3">
+                    {status.map((items, index) => (<FilterBTN task={setStatus} setPageNumber={setPageNumber} key={index} name="status" index={index} items={items} />))}
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Status
